cf05f2b6ad595b488cafdabc5cf8cb9975f1202e
nekonamic <pcemiria@gmail.com>
2024-10-29 07:56:51 +0000

    starter.md

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,7 +11,7 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	author: 'nekonamic', // Site author
 	title: 'nekonamic的博客', // Site title.
-	description: '还没想好', // Description to display in the meta tags
+	description: 'nekonamic的博客，内有关于随笔，技术与教程', // Description to display in the meta tags
 	lang: 'zh-CN',
 	ogLocale: 'zh-CN',
 	shareMessage: '分享这篇文章', // Message to share a post on social media
