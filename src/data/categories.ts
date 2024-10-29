32c2b7c2616c50e1105d2a059d8fd74dc6146845
nekonamic <pcemiria@gmail.com>
2024-10-29 06:08:54 +0000

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,6 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'随笔',
+	'技术',
+	'游戏'
+] as const
---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+	paginationSize: number
+}
+
+export const siteConfig: SiteConfig = {
+	author: 'nekonamic', // Site author
+	title: 'nekonamic的博客', // Site title.
+	description: '还没想好', // Description to display in the meta tags
+	lang: 'zh-CN',
+	ogLocale: 'zh-CN',
+	shareMessage: '分享这篇文章', // Message to share a post on social media
+	paginationSize: 6 // Number of posts per page
+}
